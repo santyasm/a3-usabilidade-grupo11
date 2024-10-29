72661549176611ca5022077538c19418edfa4a0e
santyasm <yyasant5@gmail.com>
2024-10-28 22:15:18 -0300

    initial commit

---
+++ app/src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <h1>Hospital Infantil São Judas Tadeu</h1>;
+}
+
+export default App;
---
+++ app/src/theme/index.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    primary: "#29367D",
+    // secondary: "#f7f7f7",
+    // background: "#f9f9f9",
+    // text: "#333",
+    // link: "#007bff",
+    // danger: "#dc3545",
+    // success: "#28a745",
+    // warning: "#ffc107",
+  },
+};
