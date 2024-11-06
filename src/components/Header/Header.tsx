bbff4e72b8a2fba2c611e3cb3798aa9587becca2
santyasm <yyasant5@gmail.com>
2024-11-06 14:19:15 -0300

    tailwindcss added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <div className="flex px-5 items-center bg-blue-800 min-h-20 justify-between">
+      <h1 className="text-white text-2xl font-semibold">
+        Hospital Infantil são Judas Tadeu 🏥
+      </h1>
+      <h1 className="text-white text-xl font-semibold">
+        Consultas e exames: (11) 4433-6767
+      </h1>
+    </div>
+  );
+};
